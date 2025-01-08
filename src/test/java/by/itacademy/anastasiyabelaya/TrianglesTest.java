fb687535a69cbefcc413a397d9ecc81feaafa8a4
Anastasiya Belaya <anastasia-belaya@mail.ru>
2025-01-08 18:49:29 +0300

    add format to TrianglesTest methods

--- src/test/java/by/itacademy/anastasiyabelaya/TrianglesTest.java
+++ src/test/java/by/itacademy/anastasiyabelaya/TrianglesTest.java
@@ -7,36 +7,36 @@ public class TrianglesTest {
     @Test
     public void testTriangleHasNegativeSide() {
         String result = Triangles.defineTriangle(-1, 5, 5);
-        Assertions.assertEquals("Треугольник a=..., b=..., c=... не является треугольником", result);
+        Assertions.assertEquals(String.format("Треугольник a=%d, b=%d, c=%d не является треугольником", -1, 5, 5), result);
     }
 
     @Test
     public void testTriangleHasZeroSide() {
         String result = Triangles.defineTriangle(0, 5, 5);
-        Assertions.assertEquals("Треугольник a=..., b=..., c=... не является треугольником", result);
+        Assertions.assertEquals(String.format("Треугольник a=%d, b=%d, c=%d не является треугольником", 0, 5, 5), result);
     }
 
     @Test
     public void testTriangleIsValid() {
         String result = Triangles.defineTriangle(1, 2, 3);
-        Assertions.assertEquals("Треугольник a=..., b=..., c=... не является треугольником", result);
+        Assertions.assertEquals(String.format("Треугольник a=%d, b=%d, c=%d не является треугольником", 1, 2, 3), result);
     }
 
     @Test
     public void testTriangleIsEquilateral() {
         String result = Triangles.defineTriangle(5, 5, 5);
-        Assertions.assertEquals("Треугольник a=..., b=..., c=... - равносторонний треугольник", result);
+        Assertions.assertEquals(String.format("Треугольник a=%d, b=%d, c=%d - равносторонний треугольник", 5, 5, 5), result);
     }
 
     @Test
     public void testTriangleIsIsosceles() {
         String result = Triangles.defineTriangle(5, 5, 8);
-        Assertions.assertEquals("Треугольник a=..., b=..., c=... - равнобедренный треугольник", result);
+        Assertions.assertEquals(String.format("Треугольник a=%d, b=%d, c=%d - равнобедренный треугольник", 5, 5, 8), result);
     }
 
     @Test
     public void testTriangleIsRight() {
         String result = Triangles.defineTriangle(3, 4, 5);
-        Assertions.assertEquals("Треугольник a=..., b=..., c=... - прямогульный треугольник", result);
+        Assertions.assertEquals(String.format("Треугольник a=%d, b=%d, c=%d - прямоугольный треугольник", 3, 4, 5), result);
     }
 }
